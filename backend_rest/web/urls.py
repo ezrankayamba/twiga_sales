24f5acb8b4138f3d3a76dda10fa07c236d5e713a
ezrankayamba <ezrankayamba@gmail.com>
2020-04-04 16:17:21 +0300

    Fixed media url pattern issue

--- backend_rest/web/urls.py
+++ backend_rest/web/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^(?:(?!(api|media)).)*$', views.index),
+    re_path(r'^(?:(?!api)(?!media).)*$', views.index),
 ]
