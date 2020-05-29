a4edfb29751069170c480a3667e328b8552963f6
ezrankayamba <ezrankayamba@gmail.com>
2020-05-29 11:35:44 +0300

    Fix export issue

--- frontend_web/src/_helpers/FileDownload.js
+++ frontend_web/src/_helpers/FileDownload.js
@@ -1,3 +1,11 @@
+function download(content, filename, contentType) {
+  if (!contentType) contentType = "application/octet-stream";
+  var a = document.createElement("a");
+  var blob = new Blob([content], { type: contentType });
+  a.href = window.URL.createObjectURL(blob);
+  a.download = filename;
+  a.click();
+}
 const FileDownload = {
   get: (res, name = "Export") => {
     let headers = {};
