dc0efadda58817808f17f0671612684e768bf731
ezrankayamba <ezrankayamba@gmail.com>
2021-03-10 09:59:28 +0300

    Fix docs mismatch check

--- backend_rest/sales/reports.py
+++ backend_rest/sales/reports.py
@@ -39,7 +39,7 @@ def get_sales(q):
     elif q == 'nodocs_new':
         return qs.filter(doc_count__lt=2, transaction_date__gt=threshold_date())
     elif q == 'docs_nomatch':
-        return qs.filter(doc_count=2).filter(~Q(total_value=F('total_value2'))).filter(~Q(quantity=F('quantity2')))
+        return qs.filter(doc_count=2).filter(~Q(total_value=F('total_value2')) | ~Q(quantity=F('quantity2')))
     else:
         return qs.filter(doc_count=2)
 
